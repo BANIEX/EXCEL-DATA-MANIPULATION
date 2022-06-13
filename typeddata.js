5708f7f374d65c26452d30591613a40cd07a4cfc
EGUNJOBI OLABANJI <BANJIBANIEX1@GMAIL.COM>
2022-06-13 16:28:08 +0100

    Update typeddata.js

--- typeddata.js
+++ typeddata.js
@@ -3,7 +3,7 @@ let csvBtn = document.querySelector("#csv-button");
 
 const fetchContacts = async () => {
   const url =
-    "https://script.googleusercontent.com/macros/echo?user_content_key=E3CVKUEg5N4oykJrbnPtheLMd8Ft0ifQ1Ly2lvGmN7qWc3HPNH9HnCI68-x-KYCK63sKSGN4V-EwxoYXKBHUzlcDDav4dF9xm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBfaEiyD8ASCT_NWu9uJBYEhKdEN-I354QK8XcksEa_YW2xfiQ5ILg04KPcR0q27pRS7xiu4BGT2VeodIqIma30teHllEL3Aptz9Jw9Md8uu&lib=M0RJuE2VVxuWLWyO6KVkgzVNpBywcptcy";
+    "https://script.googleusercontent.com/macros/echo?user_content_key=";
 
   const resp = await fetch(url);
   return resp.json()
